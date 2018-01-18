b7389bf7ad36f7d75a34dc99ed54ba6c1e885491
Grzegorz Lewczuk <lewczuk.grzeg@gmail.com>
2018-01-18 21:21:19 +0100

    Add random data and visualisation to model Added simple arg parser

--- highway/main.py
+++ highway/main.py
@@ -1,8 +1,22 @@
 #!/usr/bin/bash
+import argparse
+
+from highway.model.Model import Model
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(description='Highway system')
+    parser.add_argument('cities', type=str, help='filename with cities positions')
+    parser.add_argument('k', type=int, help='number of highway points')
+    parser.add_argument('d', type=float, help='minimal distance between exits')
+
+    args = parser.parse_args()
+
+    cities = Model.parse_file_with_cities(args.cities)
+    model = Model(cities, args.k, args.d)
+
+    model.randomize()
+    print(model)
 
 
 if __name__ == '__main__':
--- highway/model/Model.py
+++ highway/model/Model.py
@@ -1,7 +1,13 @@
+import random as rnd
+
+
 class Point:
     def __init__(self, x_: int = 0, y_: int = 0):
         self.x = x_
         self.y = y_
+
+    def __repr__(self):
+        return '({}, {})'.format(self.x, self.y)
 
 
 class HighwayPoint:
@@ -9,19 +15,57 @@ class HighwayPoint:
         self.position = position_ or Point()
         self.next = next_
 
+    def __repr__(self):
+        next_position = self.next.position if self.next else None
+
+        return '[{} -> {}]'.format(self.position, next_position)
+
 
 class HighwayExit:
     def __init__(self, position_: Point = None, city_: Point = None):
         self.position = position_ or Point()
         self.city = city_ or Point()
 
+    def __repr__(self):
+        return '{{} - {}}'.format(self.position, self.city)
+
 
 class Model:
-    def __init__(self, n, k, d):
-        self.n = n
-        self.k = k
-        self.d = d
+    MAX_X = 1000
+    MAX_Y = 1000
 
-        self.cities = [Point() for _ in range(n)]
-        self.highway = [HighwayPoint() for _ in range(k)]
+    def __init__(self, cities, highway_size, exit_distance):
+        self.cities = cities
+        self.exit_distance = exit_distance
+
+        self.highway = [HighwayPoint() for _ in range(highway_size)]
         self.exits = []
+
+    def randomize(self):
+        for i, highway_point in enumerate(self.highway):
+            highway_point.position.x = rnd.randint(0, self.MAX_X)
+            highway_point.position.y = rnd.randint(0, self.MAX_X)
+
+            allowed_indices = list(range(-1, len(self.highway)))
+            allowed_indices.remove(i)
+            random_highway_point = rnd.choice(allowed_indices)
+
+            if random_highway_point != -1:
+                highway_point.next = self.highway[random_highway_point]
+
+            else:
+                highway_point.next = None
+
+    @staticmethod
+    def parse_file_with_cities(filename):
+        cities = list()
+
+        with open(filename, 'r') as file:
+            for line in file.readlines():
+                x, y = map(int, line.split())
+                cities.append(Point(x, y))
+
+        return cities
+
+    def __str__(self):
+        return 'Cities: {}\nHighway: {}'.format(self.cities, self.highway)
