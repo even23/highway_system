93164747b0345e48f08aa76827a448ca421515bf
Grzegorz Lewczuk <lewczuk.grzeg@gmail.com>
2018-01-19 20:39:50 +0100

    tests fix

--- highway/tests/10_cities/run.sh
+++ highway/tests/10_cities/run.sh
@@ -3,4 +3,4 @@
 ./../../main.py cities 10 3 -s 10 > 10steps
 ./../../main.py cities 10 3 -s 100 > 100steps
 ./../../main.py cities 10 3 -s 1000 > 1000steps
-#./../../main.py cities 10 3 -s 10000 > 10000steps
+./../../main.py cities 10 3 -s 10000 > 10000steps
--- highway/tests/20_cities/run.sh
+++ highway/tests/20_cities/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-./../../main.py cities 20 3 -s 10 > 10steps
-./../../main.py cities 20 3 -s 100 > 100steps
-./../../main.py cities 20 3 -s 1000 > 1000steps
-./../../main.py cities 20 3 -s 10000 > 10000steps
+./../../main.py cities 10 3 -s 10 > 10steps
+./../../main.py cities 10 3 -s 100 > 100steps
+./../../main.py cities 10 3 -s 1000 > 1000steps
+./../../main.py cities 10 3 -s 10000 > 10000steps
