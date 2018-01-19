40825d3441cadb382629a06b60ff7eba3b56fd77
kaczk <even23@wp.pl>
2018-01-19 21:28:17 +0100

    changes in tests

--- highway/run_tests.sh
+++ highway/run_tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cd tests/10_cities && ./run.sh && cd ../..
-cd tests/20_cities && ./run.sh && cd ../..
-cd tests/50_cities && ./run.sh && cd ../..
+cd tests/10_cities && ./run.sh && cd ../.. && \
+cd tests/25_cities && ./run.sh && cd ../.. && \
+cd tests/50_cities && ./run.sh
